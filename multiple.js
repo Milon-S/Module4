ac05ebf71f1a7a0dfbb9dce0a03cf56c95bd5a90
Milon-S <milons.dev@gmail.com>
2026-04-18 00:40:17 +0600

    first commit

---
+++ multiple.js
@@ -0,0 +1,9 @@
+const salary = 25000;
+const isBCS = true;
+const height = 61;
+
+if (salary > 20000 && height > 60) {
+    console.log('Su-----Pattro')
+} else {
+    console.log('Onno Pattro Khuji')
+}
